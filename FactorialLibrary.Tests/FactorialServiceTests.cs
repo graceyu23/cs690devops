2215114ae927ec64f75ad6834823cbfd99b571a8
graceyu23 <graceyu23@outlook.com>
2026-05-30 08:30:30 +0000

    adding two new changes

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -8,15 +8,16 @@ public class FactorialServiceTests
     var result = FactorialService.Factorial(0);
     Assert.Equal(1,result);
   }
-}
-
-[Fact]
+  [Fact]
 public void Test_negative()
 {
   Assert.Throws<InvalidOperationException>(()=>{
     var result = FactorialService.Factorial(-1);
   });
 }
+}
+
+
 
 
 
